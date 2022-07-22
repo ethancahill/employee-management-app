0fe9f0015769e349e3077a1777ebaa91123533b1
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-21 22:07:29 -0400

    created employee model

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -1,8 +1,41 @@
 const { Schema, model } = require('mongoose');
-const {validateEmail} = require('../utils/helpers');
+const {validateEmail, validatePhoneNumber} = require('../utils/helpers');
 
 const employeeSchema = new Schema (
     {
-        
+       employeeId: {
+           type: Number,
+           unique: true,
+           required: "you must have an employee id";
+           trim: true
+       },
+       firstName: {
+           type: String,
+           required: "you must enter a first name",
+           trim: true
+       },
+       lastName: {
+           type: String,
+           required: "you must include a last name",
+           trim: true
+       },
+       email: {
+           type: String,
+           unique: true,
+           lowercase: true,
+           required: "you must enter an email address",
+           validate: [validateEmail, "Please input a valid email address"],
+       },
+       phoneNumber: {
+           type: Number,
+           unique: true,
+           required: "you must enter a phone number",
+        validate: [validatePhoneNumber, "Please input a valid phone number"],
+           trim: true
+       }
     }
-)
+);
+
+const Employee = model("Employee", employeeSchema);
+
+module.exports = Employee
