7ebc7f1776ffc72707a3e5ea9ef0adeaf06f5fec
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-25 11:36:46 -0400

    adjusted server

--- client/src/components/EditEmployee/index.js
+++ client/src/components/EditEmployee/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "../../assets/Styles/EditEmployee/EditEmployee.css";
 
 class EditEmployee extends Component {
   constructor() {
--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import "../../assets/Styles/Homepage/homepage.css"
+import "../../assets/Styles/Homepage/homepage.css";
+import BackgroundSlider from "react-background-slider";
+
+import employee1 from "../../assets/img/employee1.jpg";
+import employee2 from "../../assets/img/employee2.jpg";
+import employee3 from "../../assets/img/employee3.jpg";
+import employee4 from "../../assets/img/employee4.jpg";
+import employee5 from "../../assets/img/employee5.jpg";
+
+const imageArr = [employee1, employee2, employee3, employee4, employee5];
 
 function Homepage() {
-    return(
-        <div className="homePage">
-            <h1>Employee Management Site</h1>
-        </div>
-    )
+  return (
+    <div className="homePage">
+        <h1>Employee Management Site</h1>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
